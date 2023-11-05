82ed430190f32cc79f641c993926d06aef2f6ec0
Joachim Wiberg <troglobit@gmail.com>
2023-11-05 21:09:11 +0100

    Add 'make upload' target for easy managment
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- external.mk
+++ external.mk
@@ -1,1 +1,5 @@
 include $(sort $(wildcard $(BR2_EXTERNAL_CURIOS_PATH)/package/*/*.mk))
+
+.PHONY: upload
+upload: all
+	(cd $O/images; skopeo copy oci:rootfs-oci:edge docker://ghcr.io/kernelkit/curios:edge)
