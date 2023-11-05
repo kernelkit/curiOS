fef3d9ca24ddefeddb7f06475651103fb31df5f9
Joachim Wiberg <troglobit@gmail.com>
2023-11-05 20:57:45 +0100

    Allow curiOS to start as system container, adjust services
     - Default login: root/secret, enables SSH login
     - New rootfs overlay:
       - replace inittab with container friendly version
       - add start script for mini-snmpd + default args
     - Generate /etc/os-release with curiOS meta data
     - Generate /etc/motd and /etc/version from meta data
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

---
+++ board/container/post-build.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+tagline="curiOS — a slim curated container OS"
+
+for start in S40network S51sysrepo-plugind S52netopeer2; do
+    rm -f "${TARGET_DIR}/etc/init.d/$start"
+done
+
+if [ -z "$GIT_VERSION" ]; then
+    GIT_VERSION=$(git -C "$BR2_EXTERNAL_CURIOS_PATH" describe --always --dirty --tags)
+fi
+
+# Override VERSION in /etc/os-release and filenames for release builds
+if [ -n "$RELEASE" ]; then
+    VERSION="$RELEASE"
+else
+    VERSION=$GIT_VERSION
+fi
+
+# Buildroot original remain in /usr/lib/os-release
+rm "${TARGET_DIR}/etc/os-release"
+cat <<-EOF >"${TARGET_DIR}/etc/os-release"
+	NAME="curiOS"
+	ID=curios
+	PRETTY_NAME="$tagline $VERSION"
+	ID_LIKE="buildroot"
+	VERSION="${VERSION}"
+	VERSION_ID=${VERSION}"
+	BUILD_ID="${GIT_VERSION}"
+	HOME_URL="https://github.com/kernelkit/curios"
+	VENDOR_NAME="KernelKit"
+	VENDOR_HOME="https://github.com/kernelkit"
+	SUPPORT_URL="mailto:kernelkit@googlegroups.com"
+EOF
+
+echo "$tagline $VERSION — $(date +"%b %e %H:%M %Z %Y")" > "$TARGET_DIR/etc/version"
+ln -sf version "$TARGET_DIR/etc/motd"
