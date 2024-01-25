5853ba65b5bc9202323d617a252601192037e74a
Joachim Wiberg <troglobit@gmail.com>
2024-01-25 17:21:54 +0100

    Set up some convenience aliases and helpers for shell users
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

---
+++ board/common/rootfs/etc/profile.d/convenience.sh
@@ -0,0 +1,16 @@
+alias l='ls -CF'
+alias la='ls -A'
+alias ll='ls -alF'
+alias ls='ls --color=auto'
+
+export EDITOR=/usr/bin/edit
+export VISUAL=/usr/bin/edit
+
+alias vim='vi'
+alias view='vi -R'
+alias emacs='mg'
+alias sensible-editor=edit
+alias sensible-pager=pager
+alias hd="hexdump -C"
+
+alias llping='ping -L ff02::1 -I'
